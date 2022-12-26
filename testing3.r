bd225cea48a3e9ad95231e0cb3eb511b38455a83
ali abbas rajput <aliabbasrajput@yahoo.com>
2022-12-26 13:36:42 -0800

    Now Few lines are added

--- testing3.r
+++ testing3.r
@@ -1,3 +1,10 @@
 # This is 3rd testing Repo
 print("Hello from Github")
 
+
+name = "ali abbas"
+version = "RStudio - In writing"
+
+
+print (name)
+print ("These lines has been written in RStudio")
